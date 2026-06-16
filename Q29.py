b9da34e3aaf20fc1c7e41d8e608468f613589fc1
sharmaashu12890-ashu <sharmaashu12890@gmail.com>
2026-06-16 15:47:12 +0530

    first commit

---
+++ Q29.py
@@ -0,0 +1,18 @@
+
+a=float(input("enter first number"))
+operator=input("enter operator (+, -, *, /)")
+b=float(input("enter second number"))
+if operator =="+":
+    result=a+b
+elif operator =="-":
+    result=a-b
+elif operator =="*":
+    result=a*b
+elif operator=="/":
+    if b !=0:
+        result=a/b
+    else:
+        result="cannot divided by zero"
+else:
+    result="invalid operator"
+    print("result=",result)
