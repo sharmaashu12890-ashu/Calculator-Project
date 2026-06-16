b629c25db1ad50210d16db324da39deb5d2cecb2
sharmaashu12890-ashu <sharmaashu12890@gmail.com>
2026-06-16 15:58:34 +0530

    added remainder feature

--- calculator.py
+++ calculator.py
@@ -1,18 +1,32 @@
 
 a=float(input("enter first number"))
-operator=input("enter operator (+, -, *, /)")
+operator=input("enter operator (+, -, *, /, %)")
 b=float(input("enter second number"))
 if operator =="+":
     result=a+b
+
+
 elif operator =="-":
     result=a-b
+
+
 elif operator =="*":
     result=a*b
+
+
 elif operator=="/":
     if b !=0:
         result=a/b
     else:
         result="cannot divided by zero"
+
+elif operator=="%":
+    if b !=0:
+        result=a%b
+    else:
+     result="cannot divided by zero"
+
+
 else:
-    result="invalid operator"
-    print("result=",result)
+ result="invalid operator"
+print("result=",result)
